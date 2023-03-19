7d3c7f67974222fa7311bfb8ab2abf0b07362484
jdegern4 <jordan.degerness@gmail.com>
2023-03-19 14:21:19 -0500

    add GitHub links to portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -5,16 +5,17 @@ const Portfolio = () => {
         <section className="JD-portfolio">
             <div>
                 <span><a href="https://github.com/jdegern4/Julias_Children">Julia's Children</a></span>
-                <a href="https://arcane-brushlands-08991.herokuapp.com/"><img src={require(`../../assets/Julias-Children.jpg`)}/></a>
+                <a href="https://arcane-brushlands-08991.herokuapp.com/"><img src={require(`../../assets/Julias-Children.jpg`)} alt={`Julia's Children`}/></a>
             </div>
             <div>
-                <span><a>Get Facts and Find Cats</a></span>
+                <span><a href="https://github.com/jdegern4/Get-Facts-and-Find-Cats">Get Facts and Find Cats</a></span>
+                <a href="https://mcamy2001.github.io/Get-Facts-and-Find-Cats/"><img src={require(`../../assets/GFAFC.PNG`)} alt={`Get Facts and Find Cats`}></img></a>
             </div>
             <div>
-                <span><a>Do the Ding Dang Thing</a></span>
+                <span><a href="https://github.com/jdegern4/do-the-ding-dang-thing">Do the Ding Dang Thing</a></span>
             </div>
             <div>
-                <span><a>Social Network API</a></span>
+                <span><a href="https://github.com/jdegern4/Social-Network-API">Social Network API</a></span>
             </div>
         </section>
     )
